f884500a105f7d72e5094232149df52e33176f6b
renjer <mranjbar.z2993@gmail.com>
2018-11-07 15:02:36 +0330

    Reformat code, update readme for LDA and QDA

--- assignment2/Readme.md
+++ assignment2/Readme.md
@@ -28,12 +28,12 @@ We use the below models for our data and in each model we calculated the misclas
 with this function
 
     def calculateMisclassification(y, y_pred):
-    y = y.values
-    misclassificationSum = 0
-    for i in range(len(y)):
-        misclassificationSum +=1 if y[i] != y_pred[i] else 0
-    misclassificationError =misclassificationSum /len(y_pred)
-    return  round(misclassificationError, 4)
+        y = y.values
+        misclassificationSum = 0
+        for i in range(len(y)):
+            misclassificationSum +=1 if y[i] != y_pred[i] else 0
+        misclassificationError =misclassificationSum /len(y_pred)
+        return  round(misclassificationError, 4)
 
 and finally we compare models with the misclassification of model on trained data
     
@@ -47,17 +47,49 @@ variable and one or more nominal, ordinal, interval or ratio-level independent v
 In this model we used all of data as train data
 We calculate Logistic regression on our data and the calculated misClassification
 was :
-
-    Misclassification  = 0.0307
-
+    
+    MisClassification  = 0.0307
+    
+    Accuracy  = 0.9692532942898975
+   
  
 ## Linear Discriminant Analysis (LDA)
+Linear Discriminant Analysis (LDA) is most commonly used as dimensionality reduction technique in the pre-processing step
+ for pattern-classification and machine learning applications. The goal is to project a dataset onto a lower-dimensional 
+ space with good class-separability in order avoid overfitting (“curse of dimensionality”) and also reduce computational costs.
  
+ the misClassification for this model was:
+ 
+    MisClassification  = 0.0395
+    
+    Accuracy  = 0.9604685212298683
+    
+
+
+ 
+
+## Quadratic Discriminant Analysis (QDA) 
+QDA is not really that much different from LDA except that you assume that the covariance matrix can be different for each class and so, 
+we will estimate the covariance matrix Σk separately for each class k, k =1, 2, ... , K.
+
+QDA allows for more flexibility for the covariance matrix, tends to fit the data better than LDA,
+ but then it has more parameters to estimate. The number of parameters increases significantly with QDA. 
+ Because, with QDA, you will have a separate covariance matrix for every class. If you have many classes 
+ and not so many sample points, this can be a problem.
+    
+    MisClassification  = 0.041
+    
+    Accuracy  = 0.95900439238653
+    
+
+
 ## Conclusion
 
 ## References
-* [sklearn.linear_model.LogisticRegression](http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html)
 * [What is Logistic Regression?](https://www.statisticssolutions.com/what-is-logistic-regression/)
+* [sklearn.linear_model.LogisticRegression](http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html)
+* [Linear Discriminant Analysis ](https://sebastianraschka.com/Articles/2014_python_lda.html)
 * [sklearn.discriminant_analysis.LinearDiscriminantAnalysis](http://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html)
+* [Quadratic Discriminant Analysis (QDA)](https://onlinecourses.science.psu.edu/stat857/node/80/)
 * [sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis](http://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.QuadraticDiscriminantAnalysis.html)
 * [sklearn.naive_bayes.GaussianNB](http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html)
--- assignment2/main.py
+++ assignment2/main.py
@@ -1,110 +1,89 @@
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-import numpy as np
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
-from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis as QDA
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
-
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
 df = pd.read_csv("./breastData.csv", sep='\s*,\s*',
                  header=0, encoding='ascii', engine='python')
 
-def getFeatures():
+
+def get_features():
     features = df.columns.tolist()
     del features[10]
     del features[0]
     return features
 
-# def get_formatted_data_frame_from_predictions(X, y, predictions, params, features):
-#     newX = pd.DataFrame({"Constant": np.ones(len(X))}).join(pd.DataFrame(X))
-#     MSE = (sum((y - predictions) ** 2)) / (len(newX) - len(newX.columns))
-#     var_b = MSE * (np.linalg.inv(np.dot(newX.T, newX)).diagonal())
-#     sd_b = np.sqrt(var_b)
-#     ts_b = params / sd_b
-#     p_values = [2 * (1 - stats.t.cdf(np.abs(i), (len(newX) - 1))) for i in ts_b]
-#     sd_b = np.round(sd_b, 3)
-#     ts_b = np.round(ts_b, 3)
-#     p_values = np.round(p_values, 3)
-#     params = np.round(params, 4)
-#     myDF3 = pd.DataFrame()
-#     features.insert(0, "constants")
-#     myDF3["Feature"], myDF3["Coefficients"], myDF3["t values"], myDF3["Standard Errors"], myDF3["Probabilites"] = [
-#         features, params, ts_b, sd_b, p_values]
-#     return MSE, myDF3
 
-def saveLogisticRegression():
-    X = df[getFeatures()]
-    y = df["class"]
+def save_logistic_regression(X, y):
     logreg = LogisticRegression(random_state=0, solver='lbfgs', multi_class='multinomial').fit(X, y)
     y_pred = logreg.predict(X)
     logesticRscore = logreg.score(X, y)
     f = open("./results/LogisticRegression.txt", "w")
-    f.write("Misclassification  = " + str(calculateMisclassification(y, y_pred)) + "\n\n")
-    f.write("R-squared  = " + str(logesticRscore) + "\n\n")
+    f.write("MisClassification  = " + str(calculate_mis_classification(y, y_pred)) + "\n\n")
+    # f.write("accuracy  = " + str(metrics.accuracy_score(y, y_pred)) + "\n\n")
+    f.write("Accuracy  = " + str(logesticRscore) + "\n\n")
 
 
-def saveQda():
-    X = df[getFeatures()]
-    y = df["class"]
+def save_qda(X, y):
     qda = QDA().fit(X, y)
     y_pred = qda.predict(X)
     qdaRscore = qda.score(X, y)
     f = open("./results/Qda.txt", "w")
-    f.write("Misclassification  = " + str(calculateMisclassification(y, y_pred)) + "\n\n")
-    f.write("R-squared  = " + str(qdaRscore) + "\n\n")
+    f.write("MisClassification  = " + str(calculate_mis_classification(y, y_pred)) + "\n\n")
+    f.write("Accuracy  = " + str(qdaRscore) + "\n\n")
 
 
-def saveLda():
-    X = df[getFeatures()]
-    y = df["class"]
+def save_lda(X, y):
     lda = LDA().fit(X, y)
     y_pred = lda.predict(X)
     ldaRscore = lda.score(X, y)
     f = open("./results/Lda.txt", "w")
-    f.write("Misclassification  = " + str(calculateMisclassification(y, y_pred)) + "\n\n")
-    f.write("R-squared  = " + str(ldaRscore) + "\n\n")
+    f.write("MisClassification  = " + str(calculate_mis_classification(y, y_pred)) + "\n\n")
+    f.write("Accuracy  = " + str(ldaRscore) + "\n\n")
 
-def saveGnb():
-    X = df[getFeatures()]
-    y = df["class"]
+
+def save_gnb(X, y):
     gnb = GaussianNB().fit(X, y)
     y_pred = gnb.predict(X)
     gnbRscore = gnb.score(X, y)
     f = open("./results/gnb.txt", "w")
-    f.write("Misclassification  = " + str(calculateMisclassification(y, y_pred)) + "\n\n")
-    f.write("R-squared  = " + str(gnbRscore) + "\n\n")
+    f.write("MisClassification  = " + str(calculate_mis_classification(y, y_pred)) + "\n\n")
+    f.write("Accuracy  = " + str(gnbRscore) + "\n\n")
 
-def calculateMisclassification(y, y_pred):
+
+def calculate_mis_classification(y, y_pred):
     y = y.values
-    misclassificationSum = 0
+    misclassification_sum = 0
     for i in range(len(y)):
-        misclassificationSum +=1 if y[i] != y_pred[i] else 0
-    misclassificationError =misclassificationSum /len(y_pred)
-    return  round(misclassificationError, 4)
+        misclassification_sum += 1 if y[i] != y_pred[i] else 0
+    misclassificationError = misclassification_sum / len(y_pred)
+    return round(misclassificationError, 4)
 
+
+X = df[get_features()]
+y = df["class"]
 #### Phase1 of Assignment2
-# saveLogisticRegression()
-# saveQda()
-# saveLda()
-# saveGnb()
+save_logistic_regression(X, y)
+save_qda(X, y)
+save_lda(X, y)
+save_gnb(X, y)
 
 
 #### Phase2 of Assignment3
-def saveLinearRegression():
-    X = df[getFeatures()]
-    y = df["class"]
-    linearRegression = LinearRegression().fit(X,y)
-    y_pred = linearRegression.predict(X)
+def save_linear_regression(X, y):
+    linear_regression = LinearRegression().fit(X, y)
+    y_pred = linear_regression.predict(X)
     y_pred_classified = []
     for i in range(len(y_pred)):
-        if(y_pred[i] >3):
+        if (y_pred[i] > 3):
             y_pred_classified.append(4)
         else:
             y_pred_classified.append(2)
     f = open("./results/LinearRegression.txt", "w")
-    f.write("Misclassification  = " + str(calculateMisclassification(y, y_pred_classified)) + "\n\n")
-    f.write("R-squared  = " + str(linearRegression.score(X,y)) + "\n\n")
-saveLinearRegression()
+    f.write("MisClassification  = " + str(calculate_mis_classification(y, y_pred_classified)) + "\n\n")
+    f.write("R-squared  = " + str(linear_regression.score(X, y)) + "\n\n")
+
+
+save_linear_regression(X, y)
