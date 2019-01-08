2c68bca6233a9ef64e2c38637f3d7e80b1f3437d
renjer <mranjbar.z2993@gmail.com>
2019-01-08 20:11:30 +0330

    remove redundant codes

--- assignment5/main.py
+++ assignment5/main.py
@@ -24,16 +24,6 @@ y = df["class"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam',
-             alpha=0.0001, batch_size='auto',
-             learning_rate='constant', learning_rate_init=0.001,
-             power_t=0.5, max_iter=200, shuffle=True,
-             random_state=None, tol=0.0001, verbose=False,
-             warm_start=False, momentum=0.9,
-             nesterovs_momentum=True, early_stopping=False,
-             validation_fraction=0.1, beta_1=0.9, beta_2=0.999,
-             epsilon=1e-08, n_iter_no_change=10)
-
 clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
                     hidden_layer_sizes=(5, 2), random_state=0)
 
